a72715f4bb9a6cba2e0edb8f1556495d33acedbe
houssainiS <houssaini.hs@gmail.com>
2024-01-12 14:34:06 +0100

    started new project from 0
    deleted what i have done b4 and started everything again to learn and make project 'blog'

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,12 +6,7 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-   public function firstAction(){
-    $localname="houssaini";
-    $books= ["PHP","JAVA","CSS"];
-
-    return view('test',['name'=>$localname,'books'=>$books]);
-
-   }
+    public function testAction(){
+        return view('test');
+    }
 }
- 
--- resources/views/test.blade.php
+++
@@ -1,5 +0,0 @@
-<h1>test</h1>
-<p>SI {{$name}}</p>
-@foreach($books as $book)
-<li>{{$book}}</li>
-@endforeach
